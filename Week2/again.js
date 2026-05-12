1fa0ebc05924bc07d23d79c01b6ab2912905ece0
Shrawan Bista <sharawanbista99@gmail.com>
2026-05-12 08:08:35 +0545

    another commit

---
+++ Week2/again.js
@@ -0,0 +1,29 @@
+ function calculate(operation) {
+
+      let number1 = parseFloat(document.getElementById("num1").value);
+      let number2 = parseFloat(document.getElementById("num2").value);
+
+      let result;
+
+      if (operation == "add") {
+        result = number1 + number2;
+
+      } else if (operation == "subtract") {
+        result = number1 - number2;
+
+      } else if (operation == "multiply") {
+        result = number1 * number2;
+
+      } else if (operation == "divide") {
+
+        if (number2 == 0) {
+          result = "Cannot divide by zero";
+        } else {
+          result = number1 / number2;
+        }
+      }
+
+      document.getElementById("result").innerHTML =
+        "Result: " + Result;
+    }
+    
